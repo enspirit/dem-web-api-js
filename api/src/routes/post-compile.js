15b0d58bc0c3aaf5c70a9165dab01e90f2babe69
ygu <yoann.guyot@enspirit.be>
2022-10-10 11:06:11 +0200

    WIP

--- api/src/routes/post-compile.js
+++ api/src/routes/post-compile.js
@@ -2,26 +2,20 @@ const demCore = require('@enspirit/dem-core-rescript');
 
 async function compile(style, text, data) {
   const compilation_promise = demCore.compile(
-    {
-      template_fco: undefined, /* html template (pass undefined to use the default one shipped with dem) */
-      style_fco: { TAG: 1, _0: style },
-      text_fco: { TAG: 1, _0: text },
-      data_fco: { TAG: 1, _0: data },
-      watch_mode: false,
-      base_url_opt: undefined,
-      output_filename_opt: undefined,
-      publipost: false,
-      async: true
-    }
+    { TAG: 0, _0: undefined }, // template_fco: html template (pass undefined to use the default one shipped with dem) */
+    { TAG: 1, _0: style },     // style_fco
+    { TAG: 1, _0: text },      // text_fco
+    { TAG: 1, _0: data },      // data_fco
+    { TAG: 0, _0: undefined }  // partials
   );
   const result = await compilation_promise._0;
   return result[0];
 };
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   res.setHeader('Content-Type', 'text/plain');
 
-  const result = compile(null, null, null);
+  const result = await compile("", "", {})
   res.write(result);
   res.end(undefined);
 };
